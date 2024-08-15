97b0d1e9c95ebccfe63252a35fac64d35a0c3945
System Administrator <root@admins-MacBook-Pro.local>
2024-08-15 15:33:33 -0400

    fourth commit

---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,5 @@
+import { Redirect } from 'expo-router';
+
+export default function SignIn() {
+    return <Redirect href="https://open-mammal-92.accounts.dev/sign-in" />;
+}
---
+++ app/(auth)/sign-up.tsx
@@ -0,0 +1,5 @@
+import { Redirect } from 'expo-router';
+
+export default function SignUp() {
+    return <Redirect href="https://open-mammal-92.accounts.dev/sign-up" />;
+}
