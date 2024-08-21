3b38c9b1a25cca6b39aac39490ab75c8d23f6e52
root <root@DESKTOP-ETA9BDO>
2024-08-21 01:07:24 -0400

    2nd commit

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(auth)` | `/(auth)/sign-in` | `/(auth)/sign-up` | `/(home)` | `/(home)/` | `/(home)/PortfolioSection` | `/(home)/PortfolioSectionFull` | `/(home)/TradingSection` | `/(home)/dashboard` | `/(home)/kyc` | `/(home)/notifications` | `/(home)/profile` | `/(home)/settings` | `/(home)/support` | `/(home)/wallet` | `/(home)/watchlist` | `/(tabs)` | `/(tabs)/` | `/(tabs)/two` | `/CurrentToast` | `/PortfolioSection` | `/PortfolioSectionFull` | `/Provider` | `/TradingSection` | `/_sitemap` | `/dashboard` | `/kyc` | `/modal` | `/notifications` | `/profile` | `/settings` | `/sign-in` | `/sign-up` | `/support` | `/two` | `/wallet` | `/watchlist`;
+      StaticRoutes: `/` | `/(auth)` | `/(auth)/sign-in` | `/(auth)/sign-up` | `/(home)` | `/(home)/` | `/(home)/MarketOverview` | `/(home)/PortfolioSection` | `/(home)/PortfolioSectionFull` | `/(home)/TradingSection` | `/(home)/dashboard` | `/(home)/kyc` | `/(home)/notifications` | `/(home)/profile` | `/(home)/settings` | `/(home)/support` | `/(home)/wallet` | `/(home)/watchlist` | `/(tabs)` | `/(tabs)/` | `/(tabs)/two` | `/CurrentToast` | `/MarketOverview` | `/PortfolioSection` | `/PortfolioSectionFull` | `/Provider` | `/TradingSection` | `/_sitemap` | `/dashboard` | `/kyc` | `/modal` | `/notifications` | `/profile` | `/settings` | `/sign-in` | `/sign-up` | `/support` | `/two` | `/wallet` | `/watchlist`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
